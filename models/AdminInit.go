55d1509fd2b51a787958bb2e544767d77e58999c
yuepeng <995645653@qq.com>
2018-05-24 18:20:33 +0800

    admininit update

--- models/AdminInit.go
+++ models/AdminInit.go
@@ -4,6 +4,9 @@ import (
 	"github.com/astaxie/beego/orm"
 	"fmt"
 	"github.com/astaxie/beego"
+	"database/sql"
+	"log"
+	"os"
 )
 
 var o orm.Ormer
@@ -64,25 +67,56 @@ func Connect() {
 	}
 	orm.RegisterDataBase("default", db_type, dsn)
 }
-//创建表
+//创建数据库
 func createdb() {
 	
-}
-
-func insertUser() {
+	db_type := beego.AppConfig.String("db_type")
+	db_host := beego.AppConfig.String("db_host")
+	db_port := beego.AppConfig.String("db_port")
+	db_user := beego.AppConfig.String("db_user")
+	db_pass := beego.AppConfig.String("db_pass")
+	db_name := beego.AppConfig.String("db_name")
+	db_path := beego.AppConfig.String("db_path")
+	db_sslmode := beego.AppConfig.String("db_sslmode")
+	
+	var dsn string
+	var sqlstring string
+	switch db_type {
+	case "mysql":
+		dsn = fmt.Sprintf("%s:%s@tcp(%s:%s)/?charset=utf8", db_user, db_pass, db_host, db_port)
+		sqlstring = fmt.Sprintf("CREATE DATABASE  if not exists `%s` CHARSET utf8 COLLATE utf8_general_ci", db_name)
+		break
+	case "postgres":
+		dsn = fmt.Sprintf("host=%s  user=%s  password=%s  port=%s  sslmode=%s", db_host, db_user, db_pass, db_port, db_sslmode)
+		sqlstring = fmt.Sprintf("CREATE DATABASE %s", db_name)
+		break
+	case "sqlite3":
+		if db_path == "" {
+			db_path = "./"
+		}
+		dsn = fmt.Sprintf("%s%s.db", db_path, db_name)
+		os.Remove(dsn)
+		sqlstring = "create table init (n varchar(32));drop table init;"
+		break
+	default:
+		beego.Critical("Database driver is not allowed:", db_type)
+	}
+	db, err := sql.Open(db_type, dsn)
+	if err != nil {
+		panic(err.Error())
+	}
+	r, err := db.Exec(sqlstring)
+	if err != nil {
+		log.Println(err)
+		log.Println(r)
+	} else {
+		log.Println("Database ", db_name, " created")
+	}
+	defer db.Close()
 	
 }
 
-func insertGroup() {
-	
-}
 
-func insertRole() {
-	
-}
 
-func insertNodes()  {
-	
-}
 
 
