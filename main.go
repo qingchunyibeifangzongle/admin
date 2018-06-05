8391c353168f6e8a7c4ebdbdb98245e2fb7a0b74
qingchunyibeifangzongle <995645653@qq.com>
2018-06-05 17:13:49 +0800

    误删除了main.go

--- main.go
+++ main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"github.com/astaxie/beego"
-	//"github.com/astaxie/beego/orm"
-	"fmt"
-	"mime"
-	"os"
-	"admin/models"
-	_ "admin/routers"
+  "github.com/astaxie/beego"
+  //"github.com/astaxie/beego/orm"
+  "fmt"
+  "mime"
+  "os"
+  "admin/models"
+  _ "admin/routers"
 )
 
 const VERSION = "0.0.1"
@@ -15,37 +15,37 @@ const VERSION = "0.0.1"
 
 
 func initialize() {
-	mime.AddExtensionType(".css", "text/css")
-	//判断初始化参数
-	initArgs()
+  mime.AddExtensionType(".css", "text/css")
+  //判断初始化参数
+  initArgs()
 
-	models.Connect()
+  models.Connect()
 
 }
 func initArgs() {
-	args := os.Args
-	for _, v := range args {
-		if v == "-syncdb" {
-			models.Syncdb()
-			os.Exit(0)
-		}
-	}
+    args := os.Args
+    for _, v := range args {
+        if v == "-syncdb" {
+             models.Syncdb()
+             os.Exit(0)
+        }
+    }
 }
 
 
 func main() {
-	//初始化
-	initialize()
+  //初始化
+  initialize()
 
-	fmt.Println("Starting....")
+  fmt.Println("Starting....")
 
-	fmt.Println("Start ok")
-	// 开启 ORM 调试模式
-	//orm.Debug = true
-	// 自动建表
-	//orm.RunSyncdb("default", false, true)
-	beego.AddFuncMap("stringsToJson", models.StringsToJson)
-	//
-	// 运行时
-	beego.Run()
+  fmt.Println("Start ok")
+  // 开启 ORM 调试模式
+  //orm.Debug = true
+  // 自动建表
+  //orm.RunSyncdb("default", false, true)
+  beego.AddFuncMap("stringsToJson", models.StringsToJson)
+  //
+  // 运行时
+  beego.Run()
 }
